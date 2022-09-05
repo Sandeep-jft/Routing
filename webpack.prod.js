5df03189fda6135656f3514f67c7ebfd037c6d15
Sandeep Singh <sandeep.singh@jellyfishtechnologies.com>
2022-09-05 15:27:30 +0530

    first commit

---
+++ webpack.prod.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+
+module.exports = {
+    mode:'production',
+    entry:'./src/js/index.js',
+    output:{
+        filename:'[name].js',
+        path:path.resolve(__dirname,'dist'),
+        clean:true,
+        assetModuleFilename: 'images/[hash][ext][query]'
+
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            title:'react router app',
+            filename:'main.html',
+            minify:true //true in case of production
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)/,
+                exclude:/node_modules/,
+                use:{
+                    loader:"babel-loader",
+                    options:{
+                        presets:["@babel/preset-react","@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  // [style-loader](/loaders/style-loader)
+                  { loader: MiniCssExtractPlugin.loader },
+                  // [css-loader](/loaders/css-loader)
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      modules: true
+                    }
+                  }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  { loader: MiniCssExtractPlugin.loader },
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      modules: true
+                    }
+                  },
+                  { loader: 'sass-loader' }
+                ]
+            }
+        ]
+    }
+}
