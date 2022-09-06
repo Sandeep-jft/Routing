5c61ec5a2c14b6fadaa38571a85c5f3f660e323e
Sandeep Singh <sandeep.singh@jellyfishtechnologies.com>
2022-09-06 19:14:13 +0530

    lazy loading of component added

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HomeContainer from './container/HomeContainer/HomeContainer';
 import { Routes, Route } from 'react-router-dom';
-import AboutContainer from './container/AboutContainer/AboutContainer';
+// import AboutContainer from './container/AboutContainer/AboutContainer';
 import NavbarComponent from './components/Navbar.component';
 import NoRouteMatchContainer from './container/NoRouteMatchContainer/NoRouteMatchContainer';
 import ProductsContainer from './container/ProductsContainer/ProductsContainer';
@@ -10,6 +10,7 @@ import NewestContainer from './container/ProductsContainer/NewestContainer';
 import UsersContainer from './container/UsersContainer/UsersContainer';
 import UserDetailsContainer from './container/UserDetailsContainer/UserDetailsContainer';
 import AdminContainer from './container/AdminContainer/AdminContainer';
+const AboutContainer = React.lazy(()=>import('./container/AboutContainer/AboutContainer'))
 
 const App = () => {
   return (
@@ -17,7 +18,11 @@ const App = () => {
     <NavbarComponent />
     <Routes>
         <Route path='/' element={<HomeContainer />} />
-        <Route path='about' element={<AboutContainer />} />
+        <Route path='about' element={
+            <React.Suspense fallback={'Loading...'} >
+                <AboutContainer />
+            </React.Suspense>
+        } />
         <Route path = '*' element={<NoRouteMatchContainer />} />
         <Route path='product' element={<ProductsContainer />} >
             {/* render the features page data initially  */}
