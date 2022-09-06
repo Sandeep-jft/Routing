d79f3d0d53f561a646d78b368523d4185b9ddf8f
Sandeep Singh <sandeep.singh@jellyfishtechnologies.com>
2022-09-06 12:28:28 +0530

    update changes

--- src/js/views/ProductView/FeaturedProduct.view.jsx
+++ src/js/views/ProductView/FeaturedProduct.view.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FeatureProduct = () => {
   return (
     <div>
-        <h3>List of Feature Product</h3>
+        <h3>List of Featured Product</h3>
     </div>
   )
 }
